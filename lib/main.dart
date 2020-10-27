7659de46a1cc5f323c98cc8253e3486918051bf3
Apsaravi Gamage <apsaravigamage@gmail.com>
2020-10-27 09:09:34 +0530

    final level

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class NinjaCard extends StatelessWidget{
     return Scaffold(
       backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        title: Text('Ninja ID Card'),
+        title: Text('HR_ID_Card'),
         centerTitle: true,
         backgroundColor: Colors.grey[850],
         elevation: 0.0,
@@ -19,6 +19,16 @@ class NinjaCard extends StatelessWidget{
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start ,
           children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/1.jpg'),
+                radius: 50.0,
+              ),
+            ),
+            Divider(
+              height: 60.0,
+              color: Colors.grey[800],
+            ),
             Text(
               'NAME',
               style: TextStyle(
@@ -27,13 +37,50 @@ class NinjaCard extends StatelessWidget{
                 fontWeight: FontWeight.bold
               )
             ),
+            SizedBox(height:10.0),
             Text(
               'Chun-li',
               style: TextStyle(
                 color: Colors.amberAccent[200],
                 letterSpacing: 2.0,
                 fontSize: 28.0,
-              )
+              ),
+            ),
+            SizedBox(height:30.0),
+            Text(
+                'CURRENT HR LEVEL',
+                style: TextStyle(
+                    color: Colors.grey,
+                    letterSpacing: 2.0,
+                    fontWeight: FontWeight.bold
+                )
+            ),
+            SizedBox(height:10.0),
+            Text(
+                '8',
+                style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                )
+            ),
+            SizedBox(height: 30.0),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(width: 10.0,),
+                Text(
+                  'chun-li@nninja.co.lk',
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                  ),
+                ),
+              ],
             ),
           ],
           ),
